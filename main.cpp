0008a875d305c04345f487bbd26025106648449a
Roman Ivanov <marsianinszemli@gmail.com>
2023-11-30 20:17:47 +0300

    add funcs.cpp funcs.h main.cpp

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "funcs.h"
+using namespace std;
+int main()
+{
+    int x;
+    cout << "Выберите действие:" << endl;
+    cout << "1 - сложение" << endl;
+    cout << "2 - вычитание" << endl;
+    cout << "3 - умножение" << endl;
+    cout << "4 - деление" << endl;
+    cin >> x >> endl;
+    switch (x)
+    {
+    case 1:
+        cout << 'в разработке' << endl;
+    case 2:
+        cout << 'в разработке' << endl;
+    case 3:
+        cout << 'в разработке' << endl;
+    case 4:
+        cout << 'в разработке' << endl;
+    }
+
+system("pause");
+return 0;
+}
